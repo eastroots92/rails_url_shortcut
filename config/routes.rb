6bcd502bf76cf7745766500b6078f5505326aa4b
Eastroots92 <eastroots92@gmail.com>
2018-06-27 13:23:47 +0900

    links controller 생성 [ view : index new create ]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'links/index'
+
+  get 'links/new'
+
+  get 'links/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
