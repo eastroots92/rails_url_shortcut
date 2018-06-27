0e81410f81f3fdc14e2f6536610108e0374c1518
Eastroots92 <eastroots92@gmail.com>
2018-06-27 14:36:13 +0900

    삭제 기능 & goto 에서 유효성 검사 추가

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -16,13 +16,22 @@ class LinksController < ApplicationController
   end
 
   def destroy
+    @link = Link.find(params[:id])
+
+    @link.destroy
+
+    redirect_to "/"
   end
 
   def goto
     shortUrl = params[:short]
     longUrl = Link.find_by(short: shortUrl)
 
-    redirect_to longUrl.long
+    if longUrl.nil?
+      redirect_to "/"
+    else
+      redirect_to longUrl.long
+    end
     
   end
 end
